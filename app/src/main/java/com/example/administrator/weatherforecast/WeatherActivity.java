4dc388f8d83145dbabe973819a8ddf6326e83190
hanwei <15237457098@163.com>
2020-03-29 18:40:21 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/administrator/weatherforecast/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/weatherforecast/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.weatherforecast;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.administrator.weatherforecast.gson.Forecast;
 import com.example.administrator.weatherforecast.gson.Weather;
+import com.example.administrator.weatherforecast.service.AutoUpdateService;
 import com.example.administrator.weatherforecast.util.HttpUtil;
 import com.example.administrator.weatherforecast.util.Utility;
 
@@ -223,6 +225,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
